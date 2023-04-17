4e2380d69f6b3d3d7cb560e7250f3b9946fef73e
Bruno Lessa <brunolessa@live.com>
2023-04-16 23:29:01 -0300

    add animations

--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -9,73 +9,75 @@ export const Container = styled.div`
 `;
 
 export const LeftContainer = styled.div`
+  width: 50%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   width: 50%;
+
+  & > div {
+    & h1 {
+      font-size: 2rem;
+      font-weight: 600;
+      color: #fec142;
+    }
+
+    & h2 {
+      font-size: 1.5rem;
+      font-weight: 400;
+      color: #0f2358;
+      margin-bottom: 0.5rem;
+    }
+
+    & p {
+      font-size: 1rem;
+      font-weight: 400;
+      color: #0f2358;
+      margin-bottom: 1rem;
+    }
+
+    & button {
+      width: 15rem;
+      height: 2.5rem;
+      background-color: #fec142;
+      border: none;
+      border-radius: 30px;
+      padding-left: 1rem;
+      overflow: hidden;
+      font-size: 1rem;
+      font-weight: 600;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      color: #fff;
+      text-transform: uppercase;
+      transition: all 0.3s ease-in-out;
+      box-shadow: 0px 10px 24px 3px rgba(251, 161, 40, 0.42);
+
+      & span {
+        height: 100%;
+        width: 2.5rem;
+        color: #fec142;
+        background-color: #987327;
+        transition: all 0.3s ease-in-out;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      &:hover {
+        background-color: #987327;
+
+        & span {
+          color: #fff;
+        }
+      }
+    }
+  }
 `;
 
 export const RightContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
-
-  & h1 {
-    font-size: 2rem;
-    font-weight: 600;
-    color: #fec142;
-  }
-
-  & h2 {
-    font-size: 1.5rem;
-    font-weight: 400;
-    color: #0f2358;
-    margin-bottom: 0.5rem;
-  }
-
-  & p {
-    font-size: 1rem;
-    font-weight: 400;
-    color: #0f2358;
-    margin-bottom: 1rem;
-  }
-
-  & button {
-    width: 15rem;
-    height: 2.5rem;
-    background-color: #fec142;
-    border: none;
-    border-radius: 30px;
-    padding-left: 1rem;
-    overflow: hidden;
-    font-size: 1rem;
-    font-weight: 600;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    color: #fff;
-    text-transform: uppercase;
-    transition: all 0.3s ease-in-out;
-    box-shadow: 0px 10px 24px 3px rgba(251, 161, 40, 0.42);
-
-    & span {
-      height: 100%;
-      width: 2.5rem;
-      color: #fec142;
-      background-color: #987327;
-      transition: all 0.3s ease-in-out;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    &:hover {
-      background-color: #987327;
-
-      & span {
-        color: #fff;
-      }
-    }
-  }
+  align-items: center;
+  justify-content: center;
 `;
